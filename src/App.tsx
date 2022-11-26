5018262339ff39d4eb119272500908811158a4c7
Marcos Barranquero <marcos.barranquero@uah.es>
2022-11-26 17:25:10 +0100

    navigation added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import { Admin, Analytics, DashBoard, Landing, UserHome } from './pages'
 const App = () => {
   return (
     <BrowserRouter>
+      <Navigation />
       <Routes>
         <Route index element={<Landing />} />
         <Route path='/landing' element={<Landing />} />
@@ -13,6 +14,30 @@ const App = () => {
         <Route path='*' element={<h2>404 Not Found</h2>} />
       </Routes>
     </BrowserRouter>
+  )
+}
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to='/landing'>Landing</Link>
+        </li>
+        <li>
+          <Link to='/home'>Home</Link>
+        </li>
+        <li>
+          <Link to='/dashboard'>Dashboard</Link>
+        </li>
+        <li>
+          <Link to='/analytics'>Analytics</Link>
+        </li>
+        <li>
+          <Link to='/admin'>Admin</Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
