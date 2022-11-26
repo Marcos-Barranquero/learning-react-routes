78d38d171a690e65a15a08b8092791be482ea688
Marcos Barranquero <marcos.barranquero@uah.es>
2022-11-26 18:15:36 +0100

    default redirectTo

--- src/components/ProtectRoute.tsx
+++ src/components/ProtectRoute.tsx
@@ -2,11 +2,12 @@ import { useLogin, User } from '../loginHook'
 import { Navigate } from 'react-router-dom'
 
 type ProtectRouteProps = {
-  children: JSX.Element,
+  children: JSX.Element
   user: User | null
+  redirectTo?: string
 }
-export const ProtectedRoute = ({ children, user }: ProtectRouteProps): JSX.Element => {
-  if (!user) return <Navigate to='/landing' />
+export const ProtectedRoute = ({ children, user, redirectTo = '/landing' }: ProtectRouteProps): JSX.Element => {
+  if (!user) return <Navigate to={redirectTo} />
 
   return children
 }
