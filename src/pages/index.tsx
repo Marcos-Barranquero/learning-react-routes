5ce892208bf9b8cac9f50006601e09cdcbe1d5fb
Marcos Barranquero <marcos.barranquero@uah.es>
2022-11-26 17:22:33 +0100

    simple pages added

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export const Landing = () => <h2>Landing page (Public) </h2>
+
+export const UserHome = () => <h2>Home page (Private) </h2>
+
+export const DashBoard = () => <h2>Dashboard page (Private) </h2>
+
+export const Analytics = () => <h2>Analytics page (Private, permission: 'analize') </h2>
+
+export const Admin = () => <h2>Analytics page (Private, permission: 'admin') </h2>
